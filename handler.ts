6be5635ddf95e693b6866ff04f7580c9689bf4d7
Cao Peng <caopengau@gmail.com>
2023-09-21 10:55:41 +1000

    stripe payment

--- handler.ts
+++ handler.ts
@@ -1,9 +1,27 @@
-import { sendToUser } from "./src/sendToUser";
+import { answerPreCheckoutQuery, sendInvoice, sendPaymentOptions, sendToUser } from "./src/sendToUser";
 
 export async function hello(event) {
   const chatBody = JSON.parse(event.body!);
   const chatId = chatBody.message?.chat.id || chatBody.edited_message?.chat.id;
-  await sendToUser(chatId, "Hello from AWS Lambda!");
+  
+  const chatText = chatBody.message?.text || chatBody.edited_message?.text;
+
+  // Step 1. Ask user to select a payment option
+  if (chatText && chatText.includes('/pay')) {
+    await sendPaymentOptions(chatId, 'Please select a payment option');
+  }
+
+  // Step 2. Send invoice to user
+  if (chatBody.callback_query) {
+    const [, amountString] = chatBody.callback_query.data.split(":");
+    await sendInvoice(chatBody.callback_query.from.id, amountString);
+  }
+
+  // Step 3. Confirm checkout to allow payment
+  if (chatBody.pre_checkout_query) {
+    await answerPreCheckoutQuery(chatBody.pre_checkout_query.id);
+  }
+
   return {
     statusCode: 200,
   };
--- src/sendToUser.ts
+++ src/sendToUser.ts
@@ -13,3 +13,81 @@ export const sendToUser = async (chatId: string | number, text: string) => {
     options
   );
 };
+
+export const sendPaymentOptions = async (
+  chat_id: string | number,
+  text: string
+) => {
+  const PAYMENT_OPTIONS = [
+    [
+      {
+        text: `$2.99 for 10 usages`,
+        callback_data: `pay:2.99`,
+      },
+      {
+        text: `$4.99 for 30 usages`,
+        callback_data: `pay:4.99`,
+      },
+    ],
+    [
+      {
+        text: `$9.99 for 99 usages`,
+        callback_data: `pay:9.99`,
+      },
+    ],
+  ];
+  const options = {
+    params: {
+      parse_mode: "HTML",
+      chat_id,
+      text,
+      disable_web_page_preview: true,
+      reply_markup: JSON.stringify({
+        resize_keyboard: true,
+        inline_keyboard: PAYMENT_OPTIONS,
+      }),
+    },
+  };
+  return await axios.get(
+    `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`,
+    options
+  );
+};
+
+export const sendInvoice = async (chat_id: string | number, amount: string) => {
+  const prices = [
+    {
+      label: "Payment to bot",
+      // amount in cent, if you have a decimal price with . instead of ,
+      amount: parseInt(amount.replace(".", "")),
+    },
+  ];
+  const options = {
+    params: {
+      chat_id,
+      title: "Payment to bot",
+      description: "$" + amount,
+      payload: Date.now() + amount,
+      provider_token: process.env.STRIPE_TOKEN,
+      currency: "AUD",
+      prices: JSON.stringify(prices),
+    },
+  };
+  return await axios.get(
+    `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendInvoice`,
+    options
+  );
+};
+
+export const answerPreCheckoutQuery = async (id: string) => {
+  const options = {
+    params: {
+      pre_checkout_query_id: id,
+      ok: true,
+    },
+  };
+  return await axios.get(
+    `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/answerPreCheckoutQuery`,
+    options
+  );
+};
